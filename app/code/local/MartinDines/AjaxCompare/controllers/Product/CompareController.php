bfd6fde6e3d9fc19b88cb331cdb0e06b45ba74d1
Martin Dines <martin.dines@live.co.uk>
2013-04-12 01:14:27 +0100

    Controller override ready to handle ajax requests

---
+++ app/code/local/MartinDines/AjaxCompare/controllers/Product/CompareController.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once 'Mage/Catalog/controllers/Product/CompareController.php';
+
+class MartinDines_AjaxCompare_Product_CompareController extends Mage_Catalog_Product_CompareController {}
