a2256057277cb5e1b3bc81f81deadd804d32b0ce
Martin Dines <martin.dines@live.co.uk>
2013-04-12 01:43:17 +0100

    Todo list. Javascript test to intercept compare links

--- js/MartinDines/AjaxCompare/compare.js
+++ js/MartinDines/AjaxCompare/compare.js
@@ -1,1 +1,16 @@
 var MartinDines = MartinDines || {};
+
+window.onload = function() {
+    /*
+        IE 7 & 8 do not support getElementsByClassName() .. Support those guys or not? TBD
+     */
+    var anchors = document.getElementsByTagName('a');
+    for(var i = 0; i < anchors.length; i++) {
+        var anchor = anchors[i];
+        if ((/\blink-compare\b/).match(anchor.className)) {
+            anchor.onclick = function() {
+                console.log(anchor);
+            }
+        }
+    }
+}
