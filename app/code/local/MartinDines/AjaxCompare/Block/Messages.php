40498ddd57b16344bbc91e94ae572c13a4871fe9
Martin Dines <martin.dines@live.co.uk>
2013-04-24 01:28:10 +0100

    Wrapper added to message blocks. massive changes to javascript file. beginning to modularize. untested code in here. WIP. see @notes

--- app/code/local/MartinDines/AjaxCompare/Block/Messages.php
+++ app/code/local/MartinDines/AjaxCompare/Block/Messages.php
@@ -1,3 +1,14 @@
 <?php
 
-class MartinDines_AjaxCompare_Block_Messages extends Mage_Core_Block_Messages {}
+class MartinDines_AjaxCompare_Block_Messages extends Mage_Core_Block_Messages
+{
+    /**
+     * Wrap messages in custom div (that will always be present) so it can easily be targetted by javascript
+     * @return  string
+     */
+    public function getGroupedHtml()
+    {
+        $wrapper = '<div id="%s-container" class="ajax-message-container">%s</div>';
+        return sprintf($wrapper, $this->getNameInLayout(), parent::getGroupedHtml());
+    }
+}
--- js/MartinDines/AjaxCompare/compare.js
+++ js/MartinDines/AjaxCompare/compare.js
@@ -21,6 +21,7 @@ MartinDines.events = (function () {
             this._listeners[type].push(listener);
         },
 
+        // @todo be able to pass data to listeners
         fire: function(event) {
             if (typeof event == "string") {
                 event = { type: event };
@@ -79,37 +80,87 @@ MartinDines.utils.XMLHttpRequest = (function() {
 })(MartinDines.events.XMLHttpRequest || {});
 
 /*
+    namespace MartinDines.AjaxCompare
+ */
+MartinDines.AjaxCompare = (function() {
+    var XHR_Handler, Event_Handler;
+    var message_container_id = 'messages-container';
+    var message_ajax_url = location.protocol + "//" + location.host + '/catalog/product_compare/get_messages';
+
+    return {
+        getXHRHandler: function() { return XHR_Handler; },
+        setXHRHandler: function(handler) {
+            XHR_Handler = handler;
+        },
+        getEventHandler: function() { return Event_Handler; },
+        setEventHandler: function(handler) {
+            Event_Handler = handler;
+        },
+
+        getMessageContainerElement: function() {
+            return document.getElementById(message_container_id);
+        },
+
+        getMessages: function() {
+            var XHR = new XHR_Handler;
+            var Events = Event_Handler;
+            var URL = message_ajax_url;
+
+            XHR.open('GET', URL, true);
+            XHR.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+            XHR.send();
+
+            console.log(URL);
+            XHR.onreadystatechange = function() {
+                if (this.readyState == 2) { console.log('request made, headers available'); }
+                if (this.readyState == 3) { console.log('request in progress'); }
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        Events.fire('MartinDines_AjaxCompare_getMessages_Success');
+                    } else {
+                        Events.fire('MartinDines_AjaxCompare_getMessages_Error');
+                    }
+                }
+            }
+        },
+
+        getMessagesCallback: function() {
+            // @todo Use this as a call back after ajax has completed? Or use event
+        },
+
+        init: function() {
+            // I KNOW SOMETHING SHOULD GO HERE BUT DUNNO WAT \_("/)_/
+        }
+    };
+})();
+
+/*
     Document
  */
 
 var EventHandler = MartinDines.events;
+var XHRHandler = MartinDines.utils.XMLHttpRequest;
+
+var AjaxCompare = MartinDines.AjaxCompare;
+AjaxCompare.setEventHandler(EventHandler);
+AjaxCompare.setXHRHandler(XHRHandler);
+
 // This handler is to update message box when a product has been added
-
 EventHandler.addListener('MartinDines_AjaxCompare_AddToCompareSuccess', function() {
-    var XHR = new MartinDines.utils.XMLHttpRequest;
-    var URL = location.protocol + "//" + location.host + '/catalog/product_compare/get_messages';
-
-    XHR.open('GET', URL, true);
-    XHR.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-    XHR.send();
-
-    console.log(URL);
-    XHR.onreadystatechange = function() {
-        if (this.readyState == 2) { console.log('request made, headers available'); }
-        if (this.readyState == 3) { console.log('request in progress'); }
-        if (this.readyState == 4) {
-            // JSON is ie8+ .. check how magento handles responses and use their method
-            if (this.status == 200) {
-                var message_container = document.getElementById('messages_product_view');
-                message_container.innerHTML = this.responseText;
-                EventHandler.fire('MartinDines_AjaxCompare_AddToCompareSuccess_2');
-            }
-        }
+    var messageHtml = AjaxCompare.getMessages();
+    console.log(messageHtml);
+    if (messageHtml) {
+        var message_container = AjaxCompare.getMessageContainerElement();
+        message_container.innerHTML = messageHtml;
+        EventHandler.fire('MartinDines_AjaxCompare_AddToCompareSuccess_2');
+    } else {
+        // something went wrong
     }
-    return true;
 });
 
 // This handler is to update message box when a product has been added
+// This needs to be fixed so that it fills the target area correctly with HTML. Currently it appears to not replace
+// as expected
 EventHandler.addListener('MartinDines_AjaxCompare_AddToCompareSuccess_2', function() {
     var XHR = new MartinDines.utils.XMLHttpRequest;
     var URL = location.protocol + "//" + location.host + '/catalog/product_compare/get_sidebar_compare';
