35df076f81df5e1a0e181834c082bcc60fe2d6a7
Martin Dines <martin.dines@live.co.uk>
2013-04-25 00:49:28 +0100

    Restructured JS module to be more .. modular. Event driven and working. Still room for optimisations and more options - Such as config options, bug fixes etc

--- js/MartinDines/AjaxCompare/compare.js
+++ js/MartinDines/AjaxCompare/compare.js
@@ -84,20 +84,33 @@ MartinDines.utils.XMLHttpRequest = (function() {
 MartinDines.AjaxCompare = (function() {
     var XHR_Handler, Event_Handler;
     var message_container_id = 'messages-container';
+    var sidebar_compare_container_id = 'block-compare';
     var message_ajax_url = location.protocol + "//" + location.host + '/catalog/product_compare/get_messages';
+    var sidebar_compare_ajax_url = location.protocol + "//" + location.host + '/catalog/product_compare/get_sidebar_compare';
 
     return {
-        getXHRHandler: function() { return XHR_Handler; },
+        getXHRHandler: function() {
+            return XHR_Handler;
+        },
+
         setXHRHandler: function(handler) {
             XHR_Handler = handler;
         },
-        getEventHandler: function() { return Event_Handler; },
+
+        getEventHandler: function() {
+            return Event_Handler;
+        },
+
         setEventHandler: function(handler) {
             Event_Handler = handler;
         },
 
         getMessageContainerElement: function() {
             return document.getElementById(message_container_id);
+        },
+
+        getSidebarCompareContainerElement: function() {
+            return document.getElementById(sidebar_compare_container_id);
         },
 
         getMessages: function() {
@@ -118,14 +131,114 @@ MartinDines.AjaxCompare = (function() {
                         var XHRReponse = this.responseText;
                         Events.fire({type: 'MartinDines_AjaxCompare_getMessages_Success', data: XHRReponse});
                     } else {
-                        Events.fire('MartinDines_AjaxCompare_getMessages_Error');
+                        Events.fire('MartinDines_AjaxCompare_getMessages_Failure');
+                    }
+                }
+            }
+        },
+
+        addProductToCompare: function() {
+            var XHR = new XHR_Handler;
+            var Events = Event_Handler;
+            var element = this;
+
+            XHR.open('POST', element.href, true);
+            XHR.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+            XHR.send();
+
+            console.log(element.href);
+            XHR.onreadystatechange = function() {
+                if (this.readyState == 2) { console.log('request made, headers available'); }
+                if (this.readyState == 3) { console.log('request in progress'); }
+                if (this.readyState == 4) {
+                    // JSON is ie8+ .. check how magento handles responses and use their method
+                    var XHRResponse = JSON.parse(this.responseText)
+                    if ((this.status == 200) && (XHRResponse.success == true)) {
+                        Events.fire({type: 'MartinDines_AjaxCompare_addProductToCompare_Success', data: XHRResponse});
+                    } else {
+                        Events.fire('MartinDines_AjaxCompare_addProductToCompare_Failure');
+                    }
+                }
+            }
+            return false;
+        },
+
+        getSidebarCompare: function() {
+            var XHR = new XHR_Handler;
+            var Events = Event_Handler;
+            var URL = sidebar_compare_ajax_url;
+
+            XHR.open('GET', URL, true);
+            XHR.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+            XHR.send();
+
+            console.log(URL);
+            XHR.onreadystatechange = function() {
+                if (this.readyState == 2) { console.log('request made, headers available'); }
+                if (this.readyState == 3) { console.log('request in progress'); }
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        var XHRReponse = this.responseText;
+                        Events.fire({type: 'MartinDines_AjaxCompare_getSidebarCompare_Success', data: XHRReponse});
+                    } else {
+                        Events.fire('MartinDines_AjaxCompare_ggetSidebarCompare_Failure');
                     }
                 }
             }
         },
 
         init: function() {
-            // I KNOW SOMETHING SHOULD GO HERE BUT DUNNO WAT \_("/)_/
+            var Events = Event_Handler;
+            var URL = sidebar_compare_ajax_url;
+            var AjaxCompare = this;
+
+            // When a product has been added to compare update messages via ajax
+            Events.addListener('MartinDines_AjaxCompare_addProductToCompare_Success', function() {
+                AjaxCompare.getMessages();
+            });
+
+            // When messages request returns data - update the html with data
+            Events.addListener('MartinDines_AjaxCompare_getMessages_Success', function(event) {
+                var messageHtml = event.data;
+                console.log(event);
+                if (messageHtml) {
+                    var message_container = AjaxCompare.getMessageContainerElement();
+                    message_container.innerHTML = messageHtml;
+                }
+            });
+
+            // When messages request returns data - get sidebar compare data
+            Events.addListener('MartinDines_AjaxCompare_getMessages_Success', function() {
+                AjaxCompare.getSidebarCompare();
+            });
+
+            // When sidebar compare request returns data - update the html with data
+            Events.addListener('MartinDines_AjaxCompare_getSidebarCompare_Success', function(event) {
+                var blockHtml = event.data;
+                console.log(event);
+                if (blockHtml) {
+                    var divs = document.getElementsByTagName('div');
+                    for (var i = 0; i < divs.length; i++) {
+                        var div = divs[i];
+                        // @todo revisit because the below does not replace as expected
+                        if ((/\bblock-compare\b/).match(div.className)) { // @todo AjaxCompare.getSidebarCompareContainerElement()
+                            var responseContainer = document.createElement('div');
+                            responseContainer.innerHTML = blockHtml;
+                            div.innerHTML = blockHtml;
+                            break;
+                        }
+                    }
+                }
+            });
+
+            // @todo IE 7 & 8 do not support getElementsByClassName() .. Support those guys or not? TBD
+            var anchors = document.getElementsByTagName('a');
+            for (var i = 0; i < anchors.length; i++) {
+                var anchor = anchors[i];
+                if ((/\blink-compare\b/).match(anchor.className)) {
+                    anchor.onclick = AjaxCompare.addProductToCompare;
+                }
+            }
         }
     };
 })();
@@ -133,7 +246,6 @@ MartinDines.AjaxCompare = (function() {
 /*
     Document
  */
-
 var EventHandler = MartinDines.events;
 var XHRHandler = MartinDines.utils.XMLHttpRequest;
 
@@ -141,104 +253,6 @@ var AjaxCompare = MartinDines.AjaxCompare;
 AjaxCompare.setEventHandler(EventHandler);
 AjaxCompare.setXHRHandler(XHRHandler);
 
-// This handler is to update message box when a product has been added
-EventHandler.addListener('MartinDines_AjaxCompare_getMessages_Success', function(event) {
-    var messageHtml = event.data;
-    console.log(messageHtml);
-    if (messageHtml) {
-        var message_container = AjaxCompare.getMessageContainerElement();
-        message_container.innerHTML = messageHtml;
-        EventHandler.fire('MartinDines_AjaxCompare_AddToCompareSuccess_2');
-    } else {
-        // something went wrong
-    }
-});
-
-// This handler is to update message box when a product has been added
-// This needs to be fixed so that it fills the target area correctly with HTML. Currently it appears to not replace
-// as expected
-EventHandler.addListener('MartinDines_AjaxCompare_AddToCompareSuccess_2', function() {
-    var XHR = new MartinDines.utils.XMLHttpRequest;
-    var URL = location.protocol + "//" + location.host + '/catalog/product_compare/get_sidebar_compare';
-
-    XHR.open('GET', URL, true);
-    XHR.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-    XHR.send();
-
-    console.log(URL);
-    XHR.onreadystatechange = function() {
-        if (this.readyState == 2) { console.log('request made, headers available'); }
-        if (this.readyState == 3) { console.log('request in progress'); }
-        if (this.readyState == 4) {
-            // JSON is ie8+ .. check how magento handles responses and use their method
-            if (this.status == 200) {
-                var divs = document.getElementsByTagName('div');
-                for (var i = 0; i < divs.length; i++) {
-                    var div = divs[i];
-                    if ((/\bblock-compare\b/).match(div.className)) {
-                        var responseContainer = document.createElement('div');
-                        responseContainer.innerHTML = this.responseText;
-                        div.innerHTML = this.responseText;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-});
-
 window.onload = function() {
-    /*
-        IE 7 & 8 do not support getElementsByClassName() .. Support those guys or not? TBD
-     */
-    var anchors = document.getElementsByTagName('a');
-    for (var i = 0; i < anchors.length; i++) {
-        var anchor = anchors[i];
-        if ((/\blink-compare\b/).match(anchor.className)) {
-            anchor.onclick = function() {
-                var elem = this;
-                var XHR = new MartinDines.utils.XMLHttpRequest;
-
-                XHR.open('POST', this.href, true);
-                XHR.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-                XHR.send();
-
-                console.log(elem.href);
-                XHR.onreadystatechange = function() {
-                    if (this.readyState == 2) { console.log('request made, headers available'); }
-                    if (this.readyState == 3) { console.log('request in progress'); }
-                    if (this.readyState == 4) {
-                        // JSON is ie8+ .. check how magento handles responses and use their method
-                        var XHRResponse = JSON.parse(this.responseText);
-
-                        if (XHRResponse.success == true) {
-                            MartinDines.events.fire('MartinDines_AjaxCompare_AddToCompareSuccess');
-                        }
-                    }
-                }
-
-                // Do something with XHRResponse;
-                /*
-                var productComparePost = $.ajax(
-                    {
-                        url: settings.productCompareAddUrl,
-                        data: { product: checkbox.value },
-                        type: 'POST',
-                        beforeSend: function(jqXHR) {
-                            $.xhrPool.abortAll();
-                            $.xhrPool.push(jqXHR);
-                        },
-                        complete: function(jqXHR) {
-                            var index = $.xhrPool.indexOf(jqXHR);
-                            if (index > -1) {
-                                $.xhrPool.splice(index, 1);
-                            }
-                        }
-                    }
-                );*/
-
-                return false;
-            }
-        }
-    }
+    AjaxCompare.init();
 }
